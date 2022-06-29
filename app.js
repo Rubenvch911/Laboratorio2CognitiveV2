048e8b89e096ff1ac5c58677f2dd85b2bff963a0
Ruben Vargas <ruben.vargas@utec.edu.pe>
2022-06-29 15:50:52 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,105 @@
+const express = require('express');
+const mysql = require('mysql');
+
+const bodyParser = require('body-parser')
+
+const PORT = process.env.PORT || 3050;
+
+const app = express();
+
+app.use(bodyParser.json());
+
+//MySql
+const connection = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'root',
+        database:'node20_mysql'
+    }
+);
+
+// all customers
+app.get('/contactos',(req,res)=>
+{
+    const sql = 'SELECT * FROM contactos';
+
+    connection.query(sql,(error,results)=>{
+        if(error) throw error;
+        if(results.length>0){
+            res.json(results);
+        } else 
+        {
+            res.send('Not result');
+        }
+    });
+
+});
+
+app.get('/contactos/:id',(req,res)=>
+{
+    const { id } = req.params;
+    const sql = `SELECT * FROM contactos WHERE id = ${id}`;
+    connection.query(sql,(error,results)=>{
+        if(error) throw error;
+
+        if(results.length>0){
+            res.json(results);
+        } else 
+        {
+            res.send('Not result');
+        }
+    });
+});
+
+app.post('/add',(req,res)=>
+{
+    const sql = 'INSERT INTO contactos SET ?';
+
+    const contactoObj = {
+        name: req.body.name,
+        city: req.body.city
+    };
+
+    connection.query(sql, contactoObj,error => {
+        if(error) throw error;
+        res.send('Contacto creado!');
+    });
+});
+
+app.put('/update/:id',(req,res)=>
+{
+    const {id} = req.params;
+    const {name,city} = req.body;
+    const sql = `UPDATE contactos SET name = '${name}', city='${city}'
+    WHERE id = ${id}`;
+
+    connection.query(sql, error => {
+        if(error) throw error;
+        res.send('Contacto actualizado!');
+    });
+});
+
+app.delete('/delete/:id', (req,res)=>
+{
+    const {id} = req.params;
+    const sql = `DELETE FROM contactos WHERE id = ${id}`;
+
+    connection.query(sql, error => {
+        if(error) throw error;
+        res.send('Contacto borrado!');
+    });
+});
+
+app.get('/',(req, res) => {
+    res.send('Welcome to my API');
+})
+
+//Check connect
+
+connection.connect(error => {
+    if(error) throw error;
+    console.log('Database server running');
+})
+
+app.listen(PORT, ()=> console.log('Server running on port ${PORT}'));
